5c5db54e844d13ecf088704a5d6fca4622bb0411
MuffinsThaCat <talzisckind19@gmail.com>
2024-12-19 08:14:51 -0500

    Update types.go

--- consts/types.go
+++ consts/types.go
@@ -7,22 +7,14 @@ const (
     TransferID                   uint8 = 0
     ContractVerificationID       uint8 = 1
     ContractVerificationResultID uint8 = 2
-
     // ShuttleVM Action TypeIDs
     CreateObjectID         uint8 = 3
-    DeleteObjectID         uint8 = 4
-    ChangeObjectCodeID     uint8 = 5
-    ChangeObjectStorageID  uint8 = 6
-    SetInputObjectID       uint8 = 7
-    SendEventID           uint8 = 8
-
+    SetInputObjectID       uint8 = 4
+    SendEventID           uint8 = 5
     // ShuttleVM Result TypeIDs
-    CreateObjectResultID        uint8 = 9
-    DeleteObjectResultID        uint8 = 10
-    ChangeObjectCodeResultID    uint8 = 11
-    ChangeObjectStorageResultID uint8 = 12
-    SetInputObjectResultID      uint8 = 13
-    SendEventResultID          uint8 = 14
+    CreateObjectResultID   uint8 = 6
+    SetInputObjectResultID uint8 = 7
+    SendEventResultID     uint8 = 8
 )
 
 // ShuttleVM Error Types
@@ -30,7 +22,6 @@ var (
     ErrObjectExists     = "object already exists"
     ErrObjectNotFound   = "object not found"
     ErrInvalidID        = "invalid object ID"
-    ErrUnauthorized     = "unauthorized operation"
     ErrCodeTooLarge     = "code size exceeds maximum"
     ErrStorageTooLarge  = "storage size exceeds maximum"
     ErrInvalidFunction  = "invalid function call"
