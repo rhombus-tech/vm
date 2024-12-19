fb2845c4f6badd83dbbc66eed559a48f50e96774
MuffinsThaCat <talzisckind19@gmail.com>
2024-12-18 22:06:31 -0500

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -1,34 +1,38 @@
 // Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
-
 package consts
 
 import (
-	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/version"
+    "github.com/ava-labs/avalanchego/ids"
+    "github.com/ava-labs/avalanchego/version"
 )
 
 const (
-	HRP      = "morpheus"
-	Name     = "morpheusvm"
-	Symbol   = "RED"
-	Decimals = 9
+    HRP      = "morpheus"
+    Name     = "morpheusvm"
+    Symbol   = "RED"
+    Decimals = 9
+
+    // Size limits for ShuttleVM
+    MaxCodeSize    = 1024 * 1024    // 1MB
+    MaxStorageSize = 1024 * 1024    // 1MB
+    MaxIDLength    = 256
 )
 
 var ID ids.ID
 
 func init() {
-	b := make([]byte, ids.IDLen)
-	copy(b, []byte(Name))
-	vmID, err := ids.ToID(b)
-	if err != nil {
-		panic(err)
-	}
-	ID = vmID
+    b := make([]byte, ids.IDLen)
+    copy(b, []byte(Name))
+    vmID, err := ids.ToID(b)
+    if err != nil {
+        panic(err)
+    }
+    ID = vmID
 }
 
 var Version = &version.Semantic{
-	Major: 0,
-	Minor: 0,
-	Patch: 1,
+    Major: 0,
+    Minor: 0,
+    Patch: 1,
 }
