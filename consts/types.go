668448a0b39530183d4059e26abf68100102afad
MuffinsThaCat <talzisckind19@gmail.com>
2024-12-19 19:30:12 -0500

    Update types.go

--- consts/types.go
+++ consts/types.go
@@ -11,10 +11,14 @@ const (
     CreateObjectID         uint8 = 3
     SetInputObjectID       uint8 = 4
     SendEventID           uint8 = 5
+    CreateRegionID        uint8 = 6
+    UpdateRegionID        uint8 = 7
     // ShuttleVM Result TypeIDs
-    CreateObjectResultID   uint8 = 6
-    SetInputObjectResultID uint8 = 7
-    SendEventResultID     uint8 = 8
+    CreateObjectResultID   uint8 = 8
+    SetInputObjectResultID uint8 = 9
+    SendEventResultID      uint8 = 10
+    CreateRegionResultID   uint8 = 11
+    UpdateRegionResultID   uint8 = 12
 )
 
 // ShuttleVM Error Types
@@ -25,4 +29,7 @@ var (
     ErrCodeTooLarge     = "code size exceeds maximum"
     ErrStorageTooLarge  = "storage size exceeds maximum"
     ErrInvalidFunction  = "invalid function call"
+    ErrRegionExists     = "region already exists"
+    ErrRegionNotFound   = "region not found"
+    ErrInvalidTEE       = "invalid TEE address"
 )
