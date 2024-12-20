37405c6f2423966d6651b413df7378e9e6b39e82
MuffinsThaCat <talzisckind19@gmail.com>
2024-12-20 05:34:30 -0500

    Update errors.go

--- storage/errors.go
+++ storage/errors.go
@@ -1,11 +1,27 @@
 // Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
-
 package storage
 
 import "errors"
 
 var (
-	ErrInvalidAddress = errors.New("invalid address")
-	ErrInvalidBalance = errors.New("invalid balance")
+    // Existing errors
+    ErrInvalidAddress = errors.New("invalid address")
+    ErrInvalidBalance = errors.New("invalid balance")
+
+    // Object errors
+    ErrObjectNotFound  = errors.New("object not found")
+    ErrInvalidObjectID = errors.New("invalid object ID")
+    
+    // Event errors
+    ErrInvalidEvent     = errors.New("invalid event")
+    ErrEventQueueFull   = errors.New("event queue full")
+    
+    // Region errors
+    ErrInvalidRegion    = errors.New("invalid region")
+    ErrRegionNotFound   = errors.New("region not found")
+
+    // State errors
+    ErrInvalidState     = errors.New("invalid state")
+    ErrStateNotFound    = errors.New("state not found")
 )
