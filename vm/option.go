43254c99fb7f2fbbf20eca4b3d7fc722b2aedfa3
MuffinsThaCat <talzisckind19@gmail.com>
2024-12-30 05:59:59 -0500

    Update option.go

--- vm/option.go
+++ vm/option.go
@@ -1,28 +1,59 @@
-// Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
+// vm/option.go
 package vm
 
-import "github.com/ava-labs/hypersdk/vm"
+import (
+    "github.com/ava-labs/hypersdk/vm"
+    "github.com/ava-labs/hypersdk/api"
+)
 
-const Namespace = "controller"
+const Namespace = "morpheusvm"
 
 type Config struct {
-	Enabled bool `json:"enabled"`
+    Enabled bool `json:"enabled"`
+    // Add other configuration options
+    MaxObjectSize      uint64 `json:"maxObjectSize"`
+    MaxStorageSize     uint64 `json:"maxStorageSize"`
+    EnableTEEFeatures  bool   `json:"enableTEEFeatures"`
 }
 
 func NewDefaultConfig() Config {
-	return Config{
-		Enabled: true,
-	}
+    return Config{
+        Enabled:          true,
+        MaxObjectSize:    1024 * 1024, // 1MB
+        MaxStorageSize:   1024 * 1024, // 1MB
+        EnableTEEFeatures: true,
+    }
 }
 
+// WithCustomOptions allows configuring specific VM features
+func WithCustomOptions(config Config) vm.Option {
+    return vm.NewOption(
+        Namespace,
+        config,
+        func(v *vm.VM, cfg Config) (vm.Opt, error) {
+            if !cfg.Enabled {
+                return nil, nil
+            }
+            
+            // Configure VM with options
+            opts := []vm.Opt{
+                vm.WithVMAPIs(NewJSONRPCServer(v)),
+            }
+            
+            // Add TEE features if enabled
+            if cfg.EnableTEEFeatures {
+                opts = append(opts,
+                    vm.WithBlockSubscriptions(NewTEEVerifier()),
+                    vm.WithTxRemovedSubscriptions(NewTEECleanup()),
+                )
+            }
+            
+            return vm.NewOpt(opts...), nil
+        },
+    )
+}
+
+// Default With() function using default config
 func With() vm.Option {
-	return vm.NewOption(Namespace, NewDefaultConfig(), func(v *vm.VM, config Config) error {
-		if !config.Enabled {
-			return nil
-		}
-		vm.WithVMAPIs(jsonRPCServerFactory{})(v)
-		return nil
-	})
+    return WithCustomOptions(NewDefaultConfig())
 }
