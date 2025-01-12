54a7e72770658b41b0bea9aa772554bcd41ff744
MuffinsThaCat <talzisckind19@gmail.com>
2025-01-12 03:53:57 +0000

    setting up workload

--- tests/workload/workload.go
+++ tests/workload/workload.go
@@ -1,256 +1,383 @@
-// Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
+// tests/workload/workload.go
 package workload
 
 import (
-	"context"
-	"math"
-	"time"
+    "context"
+    "math"
+    "time"
 
-	"github.com/ava-labs/avalanchego/ids"
-	"github.com/stretchr/testify/require"
+    "github.com/ava-labs/avalanchego/ids"
+    "github.com/stretchr/testify/require"
 
-	"github.com/rhombus-tech/vm/actions"
-	"github.com/rhombus-tech/vm/consts"
-	"github.com/rhombus-tech/vm"
-	"github.com/ava-labs/hypersdk/api/indexer"
-	"github.com/ava-labs/hypersdk/api/jsonrpc"
-	"github.com/ava-labs/hypersdk/auth"
-	"github.com/ava-labs/hypersdk/chain"
-	"github.com/ava-labs/hypersdk/codec"
-	"github.com/ava-labs/hypersdk/crypto/bls"
-	"github.com/ava-labs/hypersdk/crypto/ed25519"
-	"github.com/ava-labs/hypersdk/crypto/secp256r1"
-	"github.com/ava-labs/hypersdk/fees"
-	"github.com/ava-labs/hypersdk/genesis"
-	"github.com/ava-labs/hypersdk/tests/workload"
+    "github.com/ava-labs/hypersdk/api/jsonrpc"
+    "github.com/ava-labs/hypersdk/api/indexer"
+    "github.com/ava-labs/hypersdk/auth"
+    "github.com/ava-labs/hypersdk/chain"
+    "github.com/ava-labs/hypersdk/codec"
+    "github.com/ava-labs/hypersdk/crypto/bls"
+    "github.com/ava-labs/hypersdk/crypto/ed25519"
+    "github.com/ava-labs/hypersdk/crypto/secp256r1"
+    "github.com/ava-labs/hypersdk/fees"
+    "github.com/ava-labs/hypersdk/genesis"
+
+    "github.com/rhombus-tech/vm/actions"
+    "github.com/rhombus-tech/vm/consts"
 )
 
 const (
-	initialBalance  uint64 = 10_000_000_000_000
-	txCheckInterval        = 100 * time.Millisecond
+    initialBalance  uint64 = 10_000_000_000_000
+    txCheckInterval        = 100 * time.Millisecond
 )
 
 var (
-	_              workload.TxWorkloadFactory  = (*workloadFactory)(nil)
-	_              workload.TxWorkloadIterator = (*simpleTxWorkload)(nil)
-	ed25519HexKeys                             = []string{
-		"323b1d8f4eed5f0da9da93071b034f2dce9d2d22692c172f3cb252a64ddfafd01b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa7", //nolint:lll
-		"8a7be2e0c9a2d09ac2861c34326d6fe5a461d920ba9c2b345ae28e603d517df148735063f8d5d8ba79ea4668358943e5c80bc09e9b2b9a15b5b15db6c1862e88", //nolint:lll
-	}
-	ed25519PrivKeys      = make([]ed25519.PrivateKey, len(ed25519HexKeys))
-	ed25519Addrs         = make([]codec.Address, len(ed25519HexKeys))
-	ed25519AuthFactories = make([]*auth.ED25519Factory, len(ed25519HexKeys))
+    ed25519HexKeys = []string{
+        "323b1d8f4eed5f0da9da93071b034f2dce9d2d22692c172f3cb252a64ddfafd01b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa7",
+        "8a7be2e0c9a2d09ac2861c34326d6fe5a461d920ba9c2b345ae28e603d517df148735063f8d5d8ba79ea4668358943e5c80bc09e9b2b9a15b5b15db6c1862e88",
+    }
+    ed25519PrivKeys      = make([]ed25519.PrivateKey, len(ed25519HexKeys))
+    ed25519Addrs         = make([]codec.Address, len(ed25519HexKeys))
+    ed25519AuthFactories = make([]*auth.ED25519Factory, len(ed25519HexKeys))
 )
 
 func init() {
-	for i, keyHex := range ed25519HexKeys {
-		privBytes, err := codec.LoadHex(keyHex, ed25519.PrivateKeyLen)
-		if err != nil {
-			panic(err)
-		}
-		priv := ed25519.PrivateKey(privBytes)
-		ed25519PrivKeys[i] = priv
-		ed25519AuthFactories[i] = auth.NewED25519Factory(priv)
-		addr := auth.NewED25519Address(priv.PublicKey())
-		ed25519Addrs[i] = addr
-	}
+    for i, keyHex := range ed25519HexKeys {
+        privBytes, err := codec.LoadHex(keyHex, ed25519.PrivateKeyLen)
+        if err != nil {
+            panic(err)
+        }
+        priv := ed25519.PrivateKey(privBytes)
+        ed25519PrivKeys[i] = priv
+        ed25519AuthFactories[i] = auth.NewED25519Factory(priv)
+        addr := auth.NewED25519Address(priv.PublicKey())
+        ed25519Addrs[i] = addr
+    }
 }
 
 type workloadFactory struct {
-	factories []*auth.ED25519Factory
-	addrs     []codec.Address
+    factories []*auth.ED25519Factory
+    addrs     []codec.Address
 }
 
-func New(minBlockGap int64) (*genesis.DefaultGenesis, workload.TxWorkloadFactory, *auth.PrivateKey, error) {
-	customAllocs := make([]*genesis.CustomAllocation, 0, len(ed25519Addrs))
-	for _, prefundedAddr := range ed25519Addrs {
-		customAllocs = append(customAllocs, &genesis.CustomAllocation{
-			Address: prefundedAddr,
-			Balance: initialBalance,
-		})
-	}
-	spamKey := &auth.PrivateKey{
-		Address: ed25519Addrs[0],
-		Bytes:   ed25519PrivKeys[0][:],
-	}
-	genesis := genesis.NewDefaultGenesis(customAllocs)
-	// Set WindowTargetUnits to MaxUint64 for all dimensions to iterate full mempool during block building.
-	genesis.Rules.WindowTargetUnits = fees.Dimensions{math.MaxUint64, math.MaxUint64, math.MaxUint64, math.MaxUint64, math.MaxUint64}
-	// Set all limits to MaxUint64 to avoid limiting block size for all dimensions except bandwidth. Must limit bandwidth to avoid building
-	// a block that exceeds the maximum size allowed by AvalancheGo.
-	genesis.Rules.MaxBlockUnits = fees.Dimensions{1800000, math.MaxUint64, math.MaxUint64, math.MaxUint64, math.MaxUint64}
-	genesis.Rules.MinBlockGap = minBlockGap
+func New(minBlockGap int64) (*genesis.DefaultGenesis, *workloadFactory, *auth.PrivateKey, error) {
+    customAllocs := make([]*genesis.CustomAllocation, 0, len(ed25519Addrs))
+    for _, prefundedAddr := range ed25519Addrs {
+        customAllocs = append(customAllocs, &genesis.CustomAllocation{
+            Address: prefundedAddr,
+            Balance: initialBalance,
+        })
+    }
 
-	return genesis, &workloadFactory{
-		factories: ed25519AuthFactories,
-		addrs:     ed25519Addrs,
-	}, spamKey, nil
+    spamKey := &auth.PrivateKey{
+        Address: ed25519Addrs[0],
+        Bytes:   ed25519PrivKeys[0][:],
+    }
+
+    gen := genesis.NewDefaultGenesis(customAllocs)
+    gen.Rules.WindowTargetUnits = fees.Dimensions{
+        math.MaxUint64,
+        math.MaxUint64,
+        math.MaxUint64,
+        math.MaxUint64,
+        math.MaxUint64,
+    }
+    gen.Rules.MaxBlockUnits = fees.Dimensions{
+        1800000,
+        math.MaxUint64,
+        math.MaxUint64,
+        math.MaxUint64,
+        math.MaxUint64,
+    }
+    gen.Rules.MinBlockGap = minBlockGap
+
+    return gen, &workloadFactory{
+        factories: ed25519AuthFactories,
+        addrs:     ed25519Addrs,
+    }, spamKey, nil
 }
 
-func (f *workloadFactory) NewSizedTxWorkload(uri string, size int) (workload.TxWorkloadIterator, error) {
-	cli := jsonrpc.NewJSONRPCClient(uri)
-	lcli := vm.NewJSONRPCClient(uri)
-	return &simpleTxWorkload{
-		factory: f.factories[0],
-		cli:     cli,
-		lcli:    lcli,
-		size:    size,
-	}, nil
+type JSONRPCClient struct {
+    requester *jsonrpc.JSONRPCClient
+    parser    chain.Parser
+    chainID   ids.ID
+}
+
+func NewJSONRPCClient(uri string) *JSONRPCClient {
+    return &JSONRPCClient{
+        requester: jsonrpc.NewJSONRPCClient(uri),
+        chainID:   ids.Empty,
+    }
+}
+
+type Parser struct {
+    actionParser *codec.TypeParser[chain.Action]
+    outputParser *codec.TypeParser[codec.Typed]
+    authParser   *codec.TypeParser[chain.Auth]
+    rules        chain.Rules
+}
+
+func (p *Parser) Rules(_ int64) chain.Rules {
+    return p.rules
+}
+
+func (p *Parser) ActionCodec() *codec.TypeParser[chain.Action] {
+    return p.actionParser
+}
+
+func (p *Parser) OutputCodec() *codec.TypeParser[codec.Typed] {
+    return p.outputParser
+}
+
+func (p *Parser) AuthCodec() *codec.TypeParser[chain.Auth] {
+    return p.authParser
+}
+
+func (c *JSONRPCClient) Parser(ctx context.Context) (chain.Parser, error) {
+    if c.parser != nil {
+        return c.parser, nil
+    }
+
+    actionParser := codec.NewTypeParser[chain.Action]()
+    actionParser.Register(&actions.CreateObjectAction{}, nil)
+    actionParser.Register(&actions.SendEventAction{}, nil)
+    actionParser.Register(&actions.SetInputObjectAction{}, nil)
+    actionParser.Register(&actions.Transfer{}, nil)
+
+    outputParser := codec.NewTypeParser[codec.Typed]()
+    outputParser.Register(&actions.CreateObjectResult{}, nil)
+    outputParser.Register(&actions.SendEventResult{}, nil)
+    outputParser.Register(&actions.SetInputObjectResult{}, nil)
+    outputParser.Register(&actions.TransferResult{}, nil)
+
+    authParser := codec.NewTypeParser[chain.Auth]()
+    authParser.Register(&auth.ED25519{}, auth.UnmarshalED25519)
+    authParser.Register(&auth.SECP256R1{}, auth.UnmarshalSECP256R1)
+    authParser.Register(&auth.BLS{}, auth.UnmarshalBLS)
+
+    c.parser = &Parser{
+        actionParser: actionParser,
+        outputParser: outputParser,
+        authParser:   authParser,
+    }
+
+    return c.parser, nil
+}
+
+type BalanceRequest struct {
+    Address codec.Address `json:"address"`
+}
+
+type BalanceResponse struct {
+    Amount uint64 `json:"amount"`
+}
+
+func (c *JSONRPCClient) Balance(ctx context.Context, addr codec.Address) (uint64, error) {
+    resp := new(BalanceResponse)
+    if err := c.requester.Request("morpheus.balance", &BalanceRequest{Address: addr}, resp); err != nil {
+        return 0, err
+    }
+    return resp.Amount, nil
+}
+
+type TxWorkloadIterator interface {
+    Next() bool
+    GenerateTxWithAssertion(context.Context) (*chain.Transaction, TxAssertion, error)
+}
+
+type TxAssertion func(context.Context, *require.Assertions, string)
+
+func (f *workloadFactory) NewSizedTxWorkload(uri string, size int) (TxWorkloadIterator, error) {
+    return &simpleTxWorkload{
+        factory: f.factories[0],
+        cli:     jsonrpc.NewJSONRPCClient(uri),
+        lcli:    NewJSONRPCClient(uri),
+        size:    size,
+    }, nil
 }
 
 type simpleTxWorkload struct {
-	factory *auth.ED25519Factory
-	cli     *jsonrpc.JSONRPCClient
-	lcli    *vm.JSONRPCClient
-	count   int
-	size    int
+    factory *auth.ED25519Factory
+    cli     *jsonrpc.JSONRPCClient
+    lcli    *JSONRPCClient
+    count   int
+    size    int
 }
 
 func (g *simpleTxWorkload) Next() bool {
-	return g.count < g.size
+    return g.count < g.size
 }
 
-func (g *simpleTxWorkload) GenerateTxWithAssertion(ctx context.Context) (*chain.Transaction, workload.TxAssertion, error) {
-	g.count++
-	other, err := ed25519.GeneratePrivateKey()
-	if err != nil {
-		return nil, nil, err
-	}
+func generateTransaction(
+    ctx context.Context,
+    chainID ids.ID,
+    actions []chain.Action,
+    auth chain.AuthFactory,
+    actionParser *codec.TypeParser[chain.Action],
+    authParser *codec.TypeParser[chain.Auth],
+) (*chain.Transaction, error) {
+    base := &chain.Base{
+        ChainID:   chainID,
+        Timestamp: time.Now().Unix(),
+    }
 
-	aother := auth.NewED25519Address(other.PublicKey())
-	parser, err := g.lcli.Parser(ctx)
-	if err != nil {
-		return nil, nil, err
-	}
-	_, tx, _, err := g.cli.GenerateTransaction(
-		ctx,
-		parser,
-		[]chain.Action{&actions.Transfer{
-			To:    aother,
-			Value: 1,
-		}},
-		g.factory,
-	)
-	if err != nil {
-		return nil, nil, err
-	}
+    tx := chain.NewTx(base, actions)
+    if err, _ := tx.Sign(auth, actionParser, authParser); err != nil {
+        return nil, err
+    }
 
-	return tx, func(ctx context.Context, require *require.Assertions, uri string) {
-		confirmTx(ctx, require, uri, tx.ID(), aother, 1)
-	}, nil
+    return tx, nil
 }
 
-func (f *workloadFactory) NewWorkloads(uri string) ([]workload.TxWorkloadIterator, error) {
-	blsPriv, err := bls.GeneratePrivateKey()
-	if err != nil {
-		return nil, err
-	}
-	blsPub := bls.PublicFromPrivateKey(blsPriv)
-	blsAddr := auth.NewBLSAddress(blsPub)
-	blsFactory := auth.NewBLSFactory(blsPriv)
+func (g *simpleTxWorkload) GenerateTxWithAssertion(ctx context.Context) (*chain.Transaction, TxAssertion, error) {
+    g.count++
+    other, err := ed25519.GeneratePrivateKey()
+    if err != nil {
+        return nil, nil, err
+    }
 
-	secpPriv, err := secp256r1.GeneratePrivateKey()
-	if err != nil {
-		return nil, err
-	}
-	secpPub := secpPriv.PublicKey()
-	secpAddr := auth.NewSECP256R1Address(secpPub)
-	secpFactory := auth.NewSECP256R1Factory(secpPriv)
+    aother := auth.NewED25519Address(other.PublicKey())
+    parser, err := g.lcli.Parser(ctx)
+    if err != nil {
+        return nil, nil, err
+    }
 
-	cli := jsonrpc.NewJSONRPCClient(uri)
-	networkID, _, blockchainID, err := cli.Network(context.Background())
-	if err != nil {
-		return nil, err
-	}
-	lcli := vm.NewJSONRPCClient(uri)
+    tx, err := generateTransaction(
+        ctx,
+        g.lcli.chainID,
+        []chain.Action{&actions.Transfer{
+            To:    aother,
+            Value: 1,
+        }},
+        g.factory,
+        parser.ActionCodec(),
+        parser.AuthCodec(),
+    )
+    if err != nil {
+        return nil, nil, err
+    }
 
-	generator := &mixedAuthWorkload{
-		addressAndFactories: []addressAndFactory{
-			{address: f.addrs[1], authFactory: f.factories[1]},
-			{address: blsAddr, authFactory: blsFactory},
-			{address: secpAddr, authFactory: secpFactory},
-		},
-		balance:   initialBalance,
-		cli:       cli,
-		lcli:      lcli,
-		networkID: networkID,
-		chainID:   blockchainID,
-	}
+    return tx, func(ctx context.Context, require *require.Assertions, uri string) {
+        confirmTx(ctx, require, uri, tx.ID(), aother, 1)
+    }, nil
+}
 
-	return []workload.TxWorkloadIterator{generator}, nil
+func (f *workloadFactory) NewWorkloads(uri string) ([]TxWorkloadIterator, error) {
+    blsPriv, err := bls.GeneratePrivateKey()
+    if err != nil {
+        return nil, err
+    }
+    blsPub := bls.PublicFromPrivateKey(blsPriv)
+    blsAddr := auth.NewBLSAddress(blsPub)
+    blsFactory := auth.NewBLSFactory(blsPriv)
+
+    secpPriv, err := secp256r1.GeneratePrivateKey()
+    if err != nil {
+        return nil, err
+    }
+    secpPub := secpPriv.PublicKey()
+    secpAddr := auth.NewSECP256R1Address(secpPub)
+    secpFactory := auth.NewSECP256R1Factory(secpPriv)
+
+    cli := jsonrpc.NewJSONRPCClient(uri)
+    networkID, _, blockchainID, err := cli.Network(context.Background())
+    if err != nil {
+        return nil, err
+    }
+
+    lcli := NewJSONRPCClient(uri)
+    lcli.chainID = blockchainID
+
+    generator := &mixedAuthWorkload{
+        addressAndFactories: []addressAndFactory{
+            {address: f.addrs[1], authFactory: f.factories[1]},
+            {address: blsAddr, authFactory: blsFactory},
+            {address: secpAddr, authFactory: secpFactory},
+        },
+        balance:   initialBalance,
+        cli:       cli,
+        lcli:      lcli,
+        networkID: networkID,
+        chainID:   blockchainID,
+    }
+
+    return []TxWorkloadIterator{generator}, nil
 }
 
 type addressAndFactory struct {
-	address     codec.Address
-	authFactory chain.AuthFactory
+    address     codec.Address
+    authFactory chain.AuthFactory
 }
 
 type mixedAuthWorkload struct {
-	addressAndFactories []addressAndFactory
-	balance             uint64
-	cli                 *jsonrpc.JSONRPCClient
-	lcli                *vm.JSONRPCClient
-	networkID           uint32
-	chainID             ids.ID
-	count               int
+    addressAndFactories []addressAndFactory
+    balance            uint64
+    cli               *jsonrpc.JSONRPCClient
+    lcli              *JSONRPCClient
+    networkID         uint32
+    chainID           ids.ID
+    count             int
 }
 
 func (g *mixedAuthWorkload) Next() bool {
-	return g.count < len(g.addressAndFactories)-1
+    return g.count < len(g.addressAndFactories)-1
 }
 
-func (g *mixedAuthWorkload) GenerateTxWithAssertion(ctx context.Context) (*chain.Transaction, workload.TxAssertion, error) {
-	defer func() { g.count++ }()
+func (g *mixedAuthWorkload) GenerateTxWithAssertion(ctx context.Context) (*chain.Transaction, TxAssertion, error) {
+    defer func() { g.count++ }()
 
-	sender := g.addressAndFactories[g.count]
-	receiver := g.addressAndFactories[g.count+1]
-	expectedBalance := g.balance - 1_000_000
+    sender := g.addressAndFactories[g.count]
+    receiver := g.addressAndFactories[g.count+1]
+    expectedBalance := g.balance - 1_000_000
 
-	parser, err := g.lcli.Parser(ctx)
-	if err != nil {
-		return nil, nil, err
-	}
-	_, tx, _, err := g.cli.GenerateTransaction(
-		ctx,
-		parser,
-		[]chain.Action{&actions.Transfer{
-			To:    receiver.address,
-			Value: expectedBalance,
-		}},
-		sender.authFactory,
-	)
-	if err != nil {
-		return nil, nil, err
-	}
-	g.balance = expectedBalance
+    parser, err := g.lcli.Parser(ctx)
+    if err != nil {
+        return nil, nil, err
+    }
 
-	return tx, func(ctx context.Context, require *require.Assertions, uri string) {
-		confirmTx(ctx, require, uri, tx.ID(), receiver.address, expectedBalance)
-	}, nil
+    tx, err := generateTransaction(
+        ctx,
+        g.chainID,
+        []chain.Action{&actions.Transfer{
+            To:    receiver.address,
+            Value: expectedBalance,
+        }},
+        sender.authFactory,
+        parser.ActionCodec(),
+        parser.AuthCodec(),
+    )
+    if err != nil {
+        return nil, nil, err
+    }
+    g.balance = expectedBalance
+
+    return tx, func(ctx context.Context, require *require.Assertions, uri string) {
+        confirmTx(ctx, require, uri, tx.ID(), receiver.address, expectedBalance)
+    }, nil
 }
 
 func confirmTx(ctx context.Context, require *require.Assertions, uri string, txID ids.ID, receiverAddr codec.Address, receiverExpectedBalance uint64) {
-	indexerCli := indexer.NewClient(uri)
-	success, _, err := indexerCli.WaitForTransaction(ctx, txCheckInterval, txID)
-	require.NoError(err)
-	require.True(success)
-	lcli := vm.NewJSONRPCClient(uri)
-	balance, err := lcli.Balance(ctx, receiverAddr)
-	require.NoError(err)
-	require.Equal(receiverExpectedBalance, balance)
-	txRes, _, err := indexerCli.GetTx(ctx, txID)
-	require.NoError(err)
-	// TODO: perform exact expected fee, units check, and output check
-	require.NotZero(txRes.Fee)
-	require.Len(txRes.Outputs, 1)
-	transferOutputBytes := []byte(txRes.Outputs[0])
-	require.Equal(consts.TransferID, transferOutputBytes[0])
-	reader := codec.NewReader(transferOutputBytes, len(transferOutputBytes))
-	transferOutputTyped, err := vm.OutputParser.Unmarshal(reader)
-	require.NoError(err)
-	transferOutput, ok := transferOutputTyped.(*actions.TransferResult)
-	require.True(ok)
-	require.Equal(receiverExpectedBalance, transferOutput.ReceiverBalance)
+    indexerCli := indexer.NewClient(uri)
+    success, _, err := indexerCli.WaitForTransaction(ctx, txCheckInterval, txID)
+    require.NoError(err)
+    require.True(success)
+
+    lcli := NewJSONRPCClient(uri)
+    balance, err := lcli.Balance(ctx, receiverAddr)
+    require.NoError(err)
+    require.Equal(receiverExpectedBalance, balance)
+
+    txRes, _, err := indexerCli.GetTx(ctx, txID)
+    require.NoError(err)
+    require.NotZero(txRes.Fee)
+    require.Len(txRes.Outputs, 1)
+
+    transferOutputBytes := []byte(txRes.Outputs[0])
+    require.Equal(consts.TransferID, transferOutputBytes[0])
+
+    reader := codec.NewReader(transferOutputBytes, len(transferOutputBytes))
+    transferOutputTyped, err := lcli.parser.OutputCodec().Unmarshal(reader)
+    require.NoError(err)
+
+    transferOutput, ok := transferOutputTyped.(*actions.TransferResult)
+    require.True(ok)
+    require.Equal(receiverExpectedBalance, transferOutput.ReceiverBalance)
 }
